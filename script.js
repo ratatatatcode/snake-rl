e2276f9dd30efee1f66ee3aaeaf34d07aaa8bd0d
ratatatatcode <pogisijamesmagnaye@gmail.com>
2026-01-05 23:46:02 +0800

    Initial setup
    [Next task] Learn how to create environment (canvas); Normal Snake Game; Array or Nodes

---
+++ script.js
@@ -0,0 +1,9 @@
+/* const episodes = 1
+const gamma = 0.9
+const alpha = 0.99
+const epsilon = 1
+const min_epsilon = 0.1
+
+const q_table = Array.from({ length: 5 }, () => Array(5).fill(0)); */
+
+const env = document.getElementById("canvas");
